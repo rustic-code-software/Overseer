212d5395019b249a34287d3840b85d5219b691eb
Gavin Sullivan <gavin.sullivan@gavinsullivan.net>
2026-08-03 12:23:33 +0100

    Added simple Result type and tests.

---
+++ Src/Overseer.Abstractions.Tests/ResultTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using System.ComponentModel.DataAnnotations;
+
+namespace Overseer.Abstractions.Tests
+{
+    public class ResultTests
+    {
+        [Fact]
+        public void Success_ReturnsEncapsulatedResult()
+        {
+            var result = Result<string>.Success("test");
+
+            result.Should().NotBeNull();
+            result.Succeeded.Should().BeTrue();
+            result.Value.Should().Be("test");
+        }
+        [Fact]
+        public void Fail_ReturnsErrorResult()
+        {
+            var result = Result<string>.Fail(new ValidationResult("Error"));
+
+            result.Should().NotBeNull();
+            result.Succeeded.Should().BeFalse();
+            result.Value.Should().Be(default);
+            result.Error.Should().NotBeNull();
+            result.Error.ErrorMessage.Should().Be("Error");
+        }
+        [Fact]
+        public void MapError_ReturnsMappedErrorResult()
+        {
+            var result = Result<string>.Fail(new ValidationResult("Error"));
+
+            var newResult = result.MapError<int>();
+
+            newResult.Should().NotBeNull();
+            newResult.Succeeded.Should().BeFalse();
+            newResult.Error.Should().NotBeNull();
+            newResult.Error.ErrorMessage.Should().Be("Error");
+
+            newResult.Should().BeOfType<Result<int>>();
+        }
+    }
+}
---
+++ Src/Overseer.Abstractions/Result.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Overseer.Abstractions
+{
+    public class Result<TValue>
+    {
+        public bool Succeeded { get; private set; }
+        public TValue? Value { get; private set; }
+        public ValidationResult? Error { get; private set; }
+
+        private Result() { }
+
+        public static Result<TValue> Success(TValue value) =>
+            new()
+            {
+                Succeeded = true,
+                Value = value
+            };
+        public static Result<TValue> Fail(ValidationResult error) =>
+            new()
+            {
+                Succeeded = false,
+                Value = default,
+                Error = error
+            };
+
+        public Result<TNew> MapError<TNew>() =>
+            new Result<TNew> {
+                Succeeded = false,
+                Value = default,
+                Error = this.Error
+            };
+    }
+}
